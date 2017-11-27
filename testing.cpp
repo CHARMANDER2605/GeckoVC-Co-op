c83161a0180f98215a147f583acf3fed2a62e800
geckovc <32776570+geckovc@users.noreply.github.com>
2017-11-26 19:07:59 -0800

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -2,11 +2,31 @@
 #include <string>
 #include <cmath>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-string ask = "";
+void generate();
 
-cout << "Hello, how can I help you?" << endl;
-cin >> ask;
-//Add certain commands.
+int leagth = 0;
+string YesOrNo = "";
+
+void main()
+{
+    cout << "How long should the password be?"
+    cin >> leagth;
+    cout << "Is it correct, you wanted the password to be " << leagth << " charecters long?"
+    cin >> YesOrNo;
+    if(YesOrNo == "Yes" || "yes" || "y")
+    {
+        generate();
+    }
+    else if(YesOrNo == "No" || "no" || "n")
+    {
+        main();
+    }
+}
+void generate()
+{
+
+}
