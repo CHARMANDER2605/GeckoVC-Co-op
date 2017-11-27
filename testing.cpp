e992277f292f539672f5099cbb25de59a251aa7e
geckovc <32776570+geckovc@users.noreply.github.com>
2017-11-26 18:47:18 -0800

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
-cout << "test" << endl;
+string ask = "";
+
+cout << "Hello, how can I help you?" << endl;
+cin >> ask;
+//Add certain commands.
